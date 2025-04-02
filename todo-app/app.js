caefafdea7eab39b2927d3e05449289708f5b29b
devakatepalli <saikumarkatepalli89@gmail.com>
2025-04-02 09:09:19 +0530

    Finnaly

--- todo-app/app.js
+++ todo-app/app.js
@@ -99,10 +99,13 @@ app.delete("/todos/:id", async (request, response) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
