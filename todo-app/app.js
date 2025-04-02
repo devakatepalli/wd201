fe775a3e2343b5eb5176ce3d25c1d6c13632bad8
devakatepalli <saikumarkatepalli89@gmail.com>
2025-04-02 09:27:08 +0530

    Final

--- todo-app/app.js
+++ todo-app/app.js
@@ -100,8 +100,7 @@ app.delete("/todos/:id", async (request, response) => {
 });
 
 require("dotenv").config();
-//const express = require("express");
-//const app = express();
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
